8d9b290ae18419ae92f769a4cde04f6d519a7c55
umitakbas82 <umit.akbas@hotmail.com.tr>
2024-03-11 17:47:55 +0300

    routings added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'expxense-planner',loadChildren:()=>import("./expense-planner/expense-planner.module").then(x=>x.ExpensePlannerModule)}
+];
+
+ 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
